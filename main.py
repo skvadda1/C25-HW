6aadd7bfd510905a1f32542fd0a613da48b21558
skvadda1 <156102921+skvadda1@users.noreply.github.com>
2024-12-28 11:47:01 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+a = 1
+b = 0
+c = 1
+d = 0
+e = 0
+f = 0
+g = 0
+print("a & b are:", d = a & b)
+print("b & c are:", e = b & c)
+print("b | c are:", f = b | c)
+print("e & f are:", e,f = g)
+print("d | g are:", d | g)
+
